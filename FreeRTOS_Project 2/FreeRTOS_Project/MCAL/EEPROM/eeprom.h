143941f055e20e8510e93f373cc58714ee969efc
seifalrahman <130805731+seifalrahman@users.noreply.github.com>
2024-04-13 16:57:11 +0200

    Add files via upload

---
+++ FreeRTOS_Project 2/FreeRTOS_Project/MCAL/EEPROM/eeprom.h
@@ -0,0 +1,20 @@
+/*
+ * eeprom.h
+ *
+ *  Created on: Apr 13, 2024
+ *      Author: seif alrahman
+ */
+
+#ifndef MCAL_EEPROM_EEPROM_H_
+#define MCAL_EEPROM_EEPROM_H_
+
+#include "std_types.h"
+
+#define EEPROM_EEDONE_REG    (*((volatile  uint32 *)0x400AF018 ))
+#define EEPROM_EESUPP_REG    (*((volatile  uint32 *)0x400AF01C ))
+#define EEPROM_EEBLOCK_REG   (*((volatile  uint32 *)0x400AF004 ))
+#define EEPROM_EEOFFSET_REG  (*((volatile  uint32 *)0x400AF008 ))
+#define EEPROM_EERDWRINC_REG (*((volatile  uint32 *)0x400AF014 ))
+
+
+#endif /* MCAL_EEPROM_EEPROM_H_ */
